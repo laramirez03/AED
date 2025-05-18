7d6072312d5d131e66a0e3b6137b8d35a56e1c61
laramirez03 <laramirez@frba.utn.edu.ar>
2025-05-18 18:49:54 -0300

    Agregado .gitignore y actualizado el codigo
    Agrego un archivo .gitignore por comodidad. Actualizo el codigo para que este mejor estructurado y con el trabajo hecho en grupo

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -1,31 +1,43 @@
 #include <cassert>
+#include <string>
+#include <cstring>
+
+std::string operator"" _s(const char* str, std::size_t len) {
+    return std::string(str, len);
+};
 
 int main() {
-    assert(true and true == true);
-    // assert(true and false == true); // Esto daria un error
-    
-    assert(true == 1); // Verifica que un booleano true es considerado un 1
-    assert('a' == 'a');
-    assert('a' + 1 == 'b');
-    assert('A' == 65); /* Verifica que los caracteres son tratados como su codigo ASCII
-    assert('A' + 'B' == 'AB'); // Esto da un error, la conclusión que obtengo es que el "+" no concatena
-    cadenas, sino que suma sus valores ASCII, en el caso de  'A' y 'B' estos deberían ser 65 y 66 */
-    assert('A' + 'B' == 65 + 66); // Verifica lo anterior
-    // assert('ab' + 'cd' == 'abcd');
-    // assert('a' + 'b' + 'c' + 'd' == 'abcd'); // Estos ejemplos tampoco verifican la asercion
+    assert(true == 1); //Verdadero
+    assert(false == 0); //Verdadero
+    assert(true == (3>2)); //verdadero
+    assert ((2>3) == false); //verdadero
 
-    //assert('abcd'.length() == 4);
-    assert(1u + 1u == 2u); // Suma de enteros no signados
-    assert(-1 - 1 == -2); // Resta de enteros signados
-    assert(-1 * 1 + -1 * 1 == -2);
-    // assert(sizeof("abcd") == 4);
-    /* La expresión de arriba dará un error, pese a que la expresión 'abcd' tiene,
-    efectivamente, 4 caracteres. Esto se debe a que, al final de una cadena de 
-    caracteres, siempre hay un caracter nulo que denota el final de la cadena */
-    assert(sizeof("abcd") == 5);
-    assert("abcd"[4] == '\0'); // Verificacion del caracter nulo 
-    
-    
+    assert('a'+1 == 'b');//verdadero
+    assert('C'== 67); //verdadero
+    /*assert('6' == 6); //falso */
+    assert('A' + 'B' == 65 + 66); //verdadero
+
+    assert (1u + 1u ==  2u); //Verdadero
+    assert(5u / 2u == 2u);
+    assert (3u - 1u == 2u);
+    /* assert(221u % 5u == 11u); // Falso*/
+
+    assert(2+2 == 4); // Suma entera
+    assert(4-2 == 2); // Resta entera
+    assert(2*8 == 16); // Producto entero
+    assert(10/5 == 2); // Division entera
+    assert(10%3 == 1); // Resto
+    assert(2<3);
+
+    assert((9.00/4.00) == 2.25); //verdadero
+    assert((9.50+0.50) == 10.00); //verdadero
+    assert((4.25-2.75) == 1.50); //verdadero
+    //assert((5.850*3.780)== 22.113); Falso
+
+    assert(strcat("hola", "mundo") == "holamundo"); //Verdadero
+    assert(strcat("Mi", "casa") == "Micasa"); //Verdadero
+    assert("ab"_s + "cd"_s == "abcd"_s);
+
     /* Credito Extra 6.2: Esto verifica que 10 veces 0.1 NO es lo mismo que 1.0
     assert(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 == 1.0);
     Lo más probable es que los ordenadores no puedan calcular con exactitud esta suma,
